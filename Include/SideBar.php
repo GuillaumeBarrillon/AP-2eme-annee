dc611e53acc93f90891f5289133d2b9717ecd6e1
Guillaume <guillaume.barrillon@limayrac.fr>
2023-09-28 17:00:39 +0200

    Footer And SideBar 17h

---
+++ Include/SideBar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Header</title>
+</head>
+
+<nav>
+    <h3>Navigation</h3>
+    <button><a href="../index.php">Accueil</a></button><br>
+    <button><a href="../listecommande.php">Liste commande</a></button>
+
+</nav>
+
+<body>
+    
+</body>
+</html>
