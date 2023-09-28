ef28231ae2583c10c0f1ac1b0ae5ffd5ef07d25a
nicolas <nicolas.calderan@limayrac.fr>
2023-09-28 15:19:37 +0200

    15H19

---
+++ connexion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <p>Identifient <input type="text" name="login" id="login"></p>
+    <p>Mot de passe <input type="text" name="password" id="password"></p>
+    
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>inscription</title>
+</head>
+<body>
+    <h1>Page d'inscription</h1>
+    <p>Identifient: <input type="text" name="id_user" id="login"></p>
+    <p>Mot de passe: <input type="text" name="id_user" id="password"></p>
+    <p>Confirmation mot de passe: <input type="text" name="id_user" id="password"></p>
+    
+</body>
+</html>
