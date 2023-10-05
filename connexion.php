9e192ed6ef4ee77e560327ccd04ec5efacaa9af4
nicolas <nicolas.calderan@limayrac.fr>
2023-10-05 14:57:04 +0200

    connection 14h 56

--- connexion.php
+++ connexion.php
@@ -1,3 +1,41 @@
+<?php 
+include "Include/db_functions.php";
+
+//Se connecter a db_connect
+$dbh = db_connect();
+
+$message = "";  // Message d'erreur
+
+// Récupère le contenu du formulaire
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+$submit = isset($_POST['submit']);
+
+if ($submit) {
+    
+    try {
+        // Vérifier la connexion à la base de données
+        if (!$dbh) {
+            die("<p>Erreur de connexion à la base de données</p>");
+        }
+        $sql = "select * from utilisateur where Login=:login and Mot_de_passe=:password";
+        $sth = $dbh->prepare($sql);
+        $sth->bindParam(':login', $login);
+        $sth->bindParam(':password', $password);
+        $sth->execute();
+        $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        die("Erreur lors de la requête SQL : " . $ex->getMessage());
+    }
+    if (count($rows) != 0) {
+        header("Location: listecommande.php");
+        exit();
+    } else {
+        $message = "login et/ou password invalide ";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,10 +47,13 @@
 </head>
 <body>
     <h1>Connexion</h1>
-    <p>Identifient <input type="text" name="login" id="login"></p>
-    <p>Mot de passe <input type="text" name="password" id="password"></p>
-    <a href="listecommande.php">Connexion</a>
-    <input type="reset" value="Annuler">
+    <p><?= $message; ?></p>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <p>Identifiant <input type="text" name="login" id="login"></p>
+        <p>Mot de passe <input type="text" name="password" id="password"></p>
+        <input type="submit" name="submit" value="Connexion">
+        <input type="reset" value="Annuler">
+    </form>
     <a href="index.php">Déconnexion</a>
     
 </body>
