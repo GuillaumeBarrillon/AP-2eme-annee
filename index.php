b2a3c2bf72e0893013be8d7654ba97dfe853ce9e
Guillaume <guillaume.barrillon@limayrac.fr>
2023-10-05 14:00:22 +0200

    Suppression du Header

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,13 +9,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Page Aceuil</title>
+    <title>Page Accueil</title>
 </head>
 <body>
 
     <h1>Bienvenue sur la page d'accueil</h1>
+    
     <a href="connexion.php">connexion</a>
     <a href="inscription.php">inscription</a>
-    
 </body>
 </html>
