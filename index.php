338ec716009fc07010c8e7862d5a568d802905e4
Guillaume <guillaume.barrillon@limayrac.fr>
2023-10-05 15:08:17 +0200

    Index & CSS 15:08

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>Page Accueil</title>
 </head>
-<body>
+<header>
+
+    <div class="connexion">
+        <a href="connexion.php"><i class="fa-solid fa-user"></i>Se connecter</a>
+    </div>
 
     <h1>Bienvenue sur la page d'accueil</h1>
-    
-    <a href="connexion.php">connexion</a>
-    <a href="inscription.php">inscription</a>
-    
-</body>
+
+</header>
 </html>
