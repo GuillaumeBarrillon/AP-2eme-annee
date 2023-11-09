c42aa8ca9d0e98a34b105f17b537a751fb072ca7
Guillaume <guillaume.barrillon@limayrac.fr>
2023-11-09 16:12:12 +0100

    Insert

--- listecommande.php
+++ listecommande.php
@@ -2,29 +2,39 @@
 include 'Include/db_functions.php';
 
 // Connexion à la base
-$dbh=db_connect();
+$dbh = db_connect();
 
 $submit = isset($_POST['submit']);
 
 if ($submit) {
 
-        $quantites = [];
-        foreach ($_POST as $key => $value) {
-            $exploded = explode("_", $key);
-            if ($exploded[0] == "qte") {
-                if ((int)($value) > 0) {
-                    $quantites[$exploded[1]] = (int)$value;
-                }
+    $quantites = [];
+    foreach ($_POST as $key => $value) {
+        $exploded = explode("_", $key);
+        if ($exploded[0] == "qte") {
+            if ((int)($value) > 0) {
+                $quantites[$exploded[1]] = (int)$value;
             }
         }
+    }
+
+    $type = $_POST['typeCommande'];
+
+    // Récupère le contenu des checkbox
+    if ($type == "place") {
+        $_SESSION['typeCommande'] = "sur place";
+    }
+
+    if ($type == "emporter") {
+        $_SESSION['typeCommande'] = "à emporter";
+    }
 
     if (count($quantites) > 0) {
-
         // Inserer une nouvelle commande
         $sql = "INSERT INTO commande(id_user, id_etat, date, total_commande, type_conso) VALUES (:id_user, :id_etat, SYSDATE(), :total_commande, :type_conso)";
         $stmt = $dbh->prepare($sql);
         $stmt->execute([
-            ":id_user" => 0,
+            ":id_user" => $_SESSION['user_id'],
             ":id_etat" => 0,
             ":total_commande" => 0,
             ":type_conso" => $_SESSION['typeCommande'],
@@ -32,40 +42,29 @@ if ($submit) {
 
         $idCommandeInseree = $dbh->lastInsertId();
 
-        foreach($quantites as $identifiantPlat => $quantite){
-          // Etape 1. Insert dans ligne commande
-          $sql = "INSERT INTO ligne(id_commande, id_produit, qte, total_ligne_ht) VALUES(:id_commande, :idproduit, :qte, :total_ligne_ht)";
-          $stmt = $dbh->prepare($sql);
-          $stmt->execute([
-            ":id_commande" => $idCommandeInseree,
-            ":idproduit" => $identifiantPlat,
-            ":qte" => ,
-            ":total_ligne_ht" => ,
-          ]);
+        foreach ($quantites as $identifiantPlat => $quantite) {
+            // Etape 1. Insert dans ligne commande
+            $sql = "INSERT INTO ligne(id_commande, id_produit, qte, total_ligne_ht) VALUES(:id_commande, :idproduit, :qte, :total_ligne_ht)";
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute([
+                ":id_commande" => $idCommandeInseree,
+                ":idproduit" => $identifiantPlat,
+                ":qte" => $quantite,
+                ":total_ligne_ht" => 0,
+            ]);
         }
-    }
-
-    $type = $_POST['typeCommande'];
-
-    // Récupère le contenu des checkbox
-    if ($type == "place"){
-        $_SESSION['typeCommande'] = "sur place";
-    }
-
-    if ($type == "emporter") {
-        $_SESSION['typeCommande'] = "à emporter";
     }
 }
 
 // Select table produit
- {
-    $sql="Select * FROM produit;";
+{
+    $sql = "Select * FROM produit;";
     try {
         $sth = $dbh->query($sql);
         $sth->execute();
         $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $ex) {
-        die( "<p>Erreur lors de la requête SQL : " . $ex->getMessage() . "</p>");
+        die("<p>Erreur lors de la requête SQL : " . $ex->getMessage() . "</p>");
     }
 }
 ?>
@@ -75,41 +74,41 @@ if ($submit) {
 
 <!doctype html>
 <html lang="fr">
+
 <head>
-  <meta charset="utf-8">
-  <title>Liste Commande</title>
-  <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Liste Commande</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-<form method="POST">
-    <table>
-        <th>Nom du plat</th>
-        <th>Prix</th>
-        <th>Quantité</th>
+    <form method="POST">
+        <table>
+            <th>Nom du plat</th>
+            <th>Prix</th>
+            <th>Quantité</th>
 
 
-<?php
-if (count($rows)>0) {
-    foreach ($rows as $row)
-        { 
-            echo "<tr>";
-            echo "<td>". $row["Libelle"]."</td>";
-            echo "<td><p>". $row["Prix"]."€</p></td>";
-            echo "<td> <input type='number' name='qte_{$row['ID_Produit']}' id='number'> </td>";
-            echo "</tr>";
-       }
-}
+            <?php
+            if (count($rows) > 0) {
+                foreach ($rows as $row) {
+                    echo "<tr>";
+                    echo "<td>" . $row["Libelle"] . "</td>";
+                    echo "<td><p>" . $row["Prix"] . "€</p></td>";
+                    echo "<td> <input type='number' name='qte_{$row['ID_Produit']}' id='number'> </td>";
+                    echo "</tr>";
+                }
+            }
 
-?>
-    </table>
-
+            ?>
+        </table>
 
 
- 
-  <h3>Veuillez choisir:</h3>
 
-        <input type="radio" id="surPlace" name="typeCommande" value="place" checked/> Sur place <br>
+
+        <h3>Veuillez choisir:</h3>
+
+        <input type="radio" id="surPlace" name="typeCommande" value="place" checked /> Sur place <br>
         <input type="radio" id="aEmporter" name="typeCommande" value="emporter" /> A emporter<br>
         <input type="submit" name="submit" value="Payer">
     </form>
@@ -122,4 +121,5 @@ if (count($rows)>0) {
 
 
 </body>
+
 </html>
