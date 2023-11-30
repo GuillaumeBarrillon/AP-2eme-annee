9a8888291290ddfa792f87820f7d553b20f48e1e
Guillaume <guillaume.barrillon@limayrac.fr>
2023-11-30 16:42:28 +0100

    Ajout CSS

--- Include/HorizontalTopBar.php
+++ Include/HorizontalTopBar.php
@@ -3,15 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="../css/style.css">
 
 </head>
 
 <nav>
-<div class ="horizontal_bar">
-    <p>User : <?php echo $_SESSION['user_name'];?></p>
-    <a href="deconnexion.php">Déconnexion</a>
-</div>
+    <div class="left">
+    <h4>RestoWeb</h4>
+    </div>
+    <div class="right">
+    <p class="user"><i class="fa-solid fa-user"></i><?php echo $_SESSION['user_name'];?></p>
+    <a href="deconnexion.php"><i class="fa-solid fa-right-from-bracket"></i></a>
+    </div>
+    
 </nav>
 
 <body>
