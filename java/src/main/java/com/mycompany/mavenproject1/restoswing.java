d0638fe7652428afa90bd833c609bc19e9352dc7
Guillaume <guillaume.barrillon@limayrac.fr>
2024-03-07 16:31:16 +0100

    Ajout de l'application java

--- java/src/main/java/com/mycompany/mavenproject1/restoswing.java
+++ java/src/main/java/com/mycompany/mavenproject1/restoswing.java
@@ -8,7 +8,7 @@ package com.mycompany.mavenproject1;
  *
  * @author CN503273
  */
-public class restowing {
+public class Restoswing {
 
     public static void main(String[] args) {
         List_commande list_commande = new List_commande();
