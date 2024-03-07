485382d0abd85337685ce8fc50b517d3d7efbecf
nicolas <nicolas.calderan@limayrac.fr>
2024-03-07 13:49:05 +0100

    java 13h49

--- java/src/main/java/com/mycompany/mavenproject1/list_commande.java
+++ java/src/main/java/com/mycompany/mavenproject1/list_commande.java
@@ -8,12 +8,12 @@ package com.mycompany.mavenproject1;
  *
  * @author CN503273
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class list_commande extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrame() {
+    public list_commande() {
         initComponents();
     }
 
@@ -118,20 +118,21 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(list_commande.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(list_commande.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(list_commande.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(list_commande.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new list_commande().setVisible(true);
             }
         });
     }
--- java/src/main/java/com/mycompany/mavenproject1/restowing.java
+++ java/src/main/java/com/mycompany/mavenproject1/restowing.java
@@ -8,7 +8,7 @@ package com.mycompany.mavenproject1;
  *
  * @author CN503273
  */
-public class Mavenproject1 {
+public class restowing {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
