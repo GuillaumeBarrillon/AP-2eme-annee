54e60fd0db1cf3f654b5ccae26fcb568bac2dac9
kerimatamboura <kerima.tamboura@limayrac.fr>
2024-03-14 13:18:32 +0100

    Hey

---
+++ java2/src/main/java/com/mycompany/mavenproject1/restowing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author CN503273
+ */
+public class restowing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
